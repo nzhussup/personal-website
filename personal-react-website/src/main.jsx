87e8ffe6f8fddeb5c16836902921dfc508237b56
nzhussup <nurzhanat2002@gmail.com>
2025-01-22 23:41:32 +0100

    👾 Add bootstrap icon dependency in order to try to fix the icon bug

--- personal-react-website/src/main.jsx
+++ personal-react-website/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./i18n";
 
 createRoot(document.getElementById("root")).render(
